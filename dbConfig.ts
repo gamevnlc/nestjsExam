760fc07ef01ec2fd61d0f8f846c923b3aa4b185c
Pham Xuan Huy <huypx@gearinc.com>
2025-06-06 10:17:21 +0700

    CRUD

--- dbConfig.ts
+++ dbConfig.ts
@@ -1,10 +1,11 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import {Property} from "./src/entities/property.entity";
 import {PropertyFeature} from "./src/entities/propertyFeature.entity";
+require('dotenv').config();
 
 export const pgConfig: PostgresConnectionOptions = {
     // Don't put here
-
+    url: process.env.URL,
     type: "postgres",
     port: 3306,
     entities: [Property, PropertyFeature],
