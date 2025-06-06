716d08e7d6b3670480f4980e93bfff7d696eb75b
Pham Xuan Huy <huypx@gearinc.com>
2025-06-06 10:13:24 +0700

    CRUD

--- dbConfig.ts
+++ dbConfig.ts
@@ -1,12 +1,14 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import {Property} from "./src/entities/property.entity";
+import {PropertyFeature} from "./src/entities/propertyFeature.entity";
 
 export const pgConfig: PostgresConnectionOptions = {
     // Don't put here
-    url: "postgresql://neondb_owner:npg_xy7d6mgBkhML@ep-sweet-bird-a8usb0q4-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+
     type: "postgres",
     port: 3306,
-    entities: [Property],
+    entities: [Property, PropertyFeature],
+    // entities: [__dirname + '/**/*.entity{.ts,.js}'],
     // dev mode only
     synchronize: true,
 }
